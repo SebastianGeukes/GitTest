4bdd773945615edde696365e46a65915b0c920ae
geukochea <sebastian.geukes@uni-muenster.de>
2015-02-24 12:31:02 +0100

    changed point color to red

--- Scatterplot.R
+++ Scatterplot.R
@@ -2,5 +2,5 @@ library("ggplot2")
 
 df <- data.frame(a = rnorm(100), b = rnorm(100))
 
-ggplot(df, aes(a, b)) + geom_point() + geom_smooth(method = "lm")
+ggplot(df, aes(a, b)) + geom_point(color = "red") + geom_smooth(method = "lm")
 
